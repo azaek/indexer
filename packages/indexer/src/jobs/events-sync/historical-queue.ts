286f8a0108e721fa0d5115e57a640031ed0868e4
d3or <deorrunner@gmail.com>
2023-08-09 13:55:51 -0400

    feat: increase consumer timeout

--- packages/indexer/src/jobs/events-sync/historical-queue.ts
+++ packages/indexer/src/jobs/events-sync/historical-queue.ts
@@ -15,7 +15,7 @@ export class EventsSyncHistoricalJob extends AbstractRabbitMqJobHandler {
   queueName = "events-sync-historical";
   maxRetries = 30;
   concurrency = 50;
-  consumerTimeout = 60 * 1000;
+  consumerTimeout = 60 * 3000;
   backoff = {
     type: "fixed",
     delay: 1000,
