2e92ee6e70842e5da776dc328d45bc8580e6167e
d3or <deorrunner@gmail.com>
2023-08-17 03:32:17 -0400

    feat: 74 concurrency

--- packages/indexer/src/jobs/events-sync/historical-queue.ts
+++ packages/indexer/src/jobs/events-sync/historical-queue.ts
@@ -14,7 +14,7 @@ export type EventsSyncHistoricalJobPayload = {
 export class EventsSyncHistoricalJob extends AbstractRabbitMqJobHandler {
   queueName = "events-sync-historical";
   maxRetries = 30;
-  concurrency = 300;
+  concurrency = 75;
   consumerTimeout = 60 * 3000;
   backoff = {
     type: "fixed",
